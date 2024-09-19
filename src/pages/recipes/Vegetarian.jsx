78c171a5bced215d9526fbae3b33af273bc693e0
jaimezzapata <zapataval2304@gmail.com>
2024-09-19 09:02:43 -0500

    Se adiciona contenido a la data y se consume en el componente

--- src/pages/recipes/Vegetarian.jsx
+++ src/pages/recipes/Vegetarian.jsx
@@ -1,6 +1,21 @@
+import { recipesVegetarian } from "../../data/dataRecipes"
+
 const Vegetarian = () => {
+  console.log(recipesVegetarian)
   return (
-    <div>Vegetarian</div>
+    <div>
+      {
+        recipesVegetarian.map((recipeVegetarian) => (
+          <div>
+            <h3>Nombre: {recipeVegetarian.nombre}</h3>
+            <p>Descripción: {recipeVegetarian.descripcion}</p>
+            <p>Dificultad: {recipeVegetarian.dificultad}</p>
+            <p>Tiempo: {recipeVegetarian.tiempo}</p>
+            <img src={recipeVegetarian.img} alt={recipeVegetarian.nombre} />
+          </div>
+        ))
+      }
+    </div>
   )
 }
 
