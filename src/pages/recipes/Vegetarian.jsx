37fd0371230de4701fd1936b4613b44731d8f514
jaimezzapata <zapataval2304@gmail.com>
2024-09-19 10:20:30 -0500

    Implementación de estilos en el componente de recetas vegetarianas

--- src/pages/recipes/Vegetarian.jsx
+++ src/pages/recipes/Vegetarian.jsx
@@ -1,17 +1,23 @@
+import { Link } from "react-router-dom"
 import { recipesVegetarian } from "../../data/dataRecipes"
-
+import './Vegetarian.css'
 const Vegetarian = () => {
   console.log(recipesVegetarian)
   return (
     <div>
       {
         recipesVegetarian.map((recipeVegetarian) => (
-          <div>
+          <div key={recipeVegetarian.id} className="card">
+            <img src={recipeVegetarian.img} alt={recipeVegetarian.nombre} />
             <h3>Nombre: {recipeVegetarian.nombre}</h3>
             <p>Descripción: {recipeVegetarian.descripcion}</p>
             <p>Dificultad: {recipeVegetarian.dificultad}</p>
             <p>Tiempo: {recipeVegetarian.tiempo}</p>
-            <img src={recipeVegetarian.img} alt={recipeVegetarian.nombre} />
+            <section className="botones">
+              <Link className="boton">Detalle</Link>
+              <Link className="boton">Editar</Link>
+              <Link className="boton">Eliminar</Link>
+            </section>
           </div>
         ))
       }
