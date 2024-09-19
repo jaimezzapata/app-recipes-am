2d9fc286447c230b6d2538196aa7a0fc0283c1c2
jaimezzapata <zapataval2304@gmail.com>
2024-09-19 08:27:38 -0500

    Adicion de estilos al Dashboard y al Header

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,16 +1,18 @@
 import { Link, Outlet } from "react-router-dom"
+import './Dashboard.css'
 
 const Dashboard = () => {
   return (
     <main className="dashboard">
-        <aside>
-            <Link to='vegetarian'>Vegetarian</Link>
-            <Link to='non-vegetarian'>Non Vegetarian</Link>
-        </aside>
-        <section>
-            <h2>Info de recetas</h2>
-            <Outlet />
-        </section>
+      <aside>
+        <Link to='vegetarian'>Vegetarian</Link>
+        <Link to='non-vegetarian'>Non Vegetarian</Link>
+      </aside>
+      <section>
+        <span className="glass"></span>
+        <h2>Info de recetas</h2>
+        <Outlet />
+      </section>
     </main>
   )
 }
