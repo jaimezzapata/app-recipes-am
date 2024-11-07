1348ae39a5b66669924cbfa6fdb668ce7d39955c
jaimezzapata <zapataval2304@gmail.com>
2024-11-07 08:55:57 -0500

    Implementación de método post con axios

--- src/pages/recipes/Vegetarian.jsx
+++ src/pages/recipes/Vegetarian.jsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom"
 import { useEffect, useState } from 'react'
+import axios from "axios"
+import Swal from "sweetalert2"
 // import { recipesVegetarian } from "../../data/dataRecipes"
 import './Vegetarian.css'
-let urlRecipes = 'http://localhost:3000/vegetarians'
+let urlRecipes = 'http://localhost:3000/vegetarians/'
 
 const Vegetarian = () => {
   const [stateRecipes, setStateRecipes] = useState([])
@@ -17,6 +19,31 @@ const Vegetarian = () => {
     getRecipes()
   }, [])
 
+  function confirmDelete(id, nombre) {
+    Swal.fire({
+      title: "Está seguro que desea eliminar esta receta: " + nombre,
+      text: "No se puede revertir esta acción!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Si, Eliminar!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        deleteRecipe(id)
+        Swal.fire({
+          title: "Eliminado!",
+          text: "La receta ha sido eliminada.",
+          icon: "success"
+        });
+      }
+    });
+  }
+  function deleteRecipe(id) {
+    axios.delete(urlRecipes + id)
+    getRecipes()
+  }
+
   return (
     <div className="cards">
       {
@@ -30,7 +57,7 @@ const Vegetarian = () => {
             <section className="botones">
               <Link className="boton">Detalle</Link>
               <Link className="boton">Editar</Link>
-              <Link className="boton">Eliminar</Link>
+              <button type="button" onClick={() => confirmDelete(recipeVegetarian.id, recipeVegetarian.nombre)} className="boton">Eliminar</button>
             </section>
           </div>
         ))
